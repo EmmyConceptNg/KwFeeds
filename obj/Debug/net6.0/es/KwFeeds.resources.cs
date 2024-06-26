740e6da18bc26b8b31f7025f4bca38c348f53b66
Emmy <emmyconceptng@gmail.com>
2024-06-26 19:49:44 +0100

    azure fixed

--- obj/Debug/net6.0/es/KwFeeds.resources.cs
+++ obj/Debug/net6.0/es/KwFeeds.resources.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KwFeeds")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute(" ")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f6eac43d251c5a2df33df3ccc98aa73e0c1f0d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ff7156dd90f3302907aa9ed67128a562cf5a602")]
 [assembly: System.Reflection.AssemblyProductAttribute("KwFeeds")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KwFeeds")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
