8ca1f260d028445e60eeca3fe037bd3afc798bd0
Emmy <emmyconceptng@gmail.com>
2024-07-25 09:22:43 +0100

    build

--- obj/Debug/net6.0/es/KwFeeds.resources.cs
+++ obj/Debug/net6.0/es/KwFeeds.resources.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KwFeeds")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute(" ")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4643344582be6d3ad39a0ef93310c1bd08ff84f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+425af8754a7e1b756d9d747c96dcc6b34250f011")]
 [assembly: System.Reflection.AssemblyProductAttribute("KwFeeds")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KwFeeds")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
