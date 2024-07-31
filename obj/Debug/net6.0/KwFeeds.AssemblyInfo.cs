b67841e1d5399949a108d40fc403ee9eef844121
Emmy <emmyconceptng@gmail.com>
2024-07-31 16:41:21 +0100

    new key

--- obj/Debug/net6.0/KwFeeds.AssemblyInfo.cs
+++ obj/Debug/net6.0/KwFeeds.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KwFeeds")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b45b492beca952f80c290281504d191ac9fb2b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbb08425f540cad4d631b94065f68c6d9fa30e76")]
 [assembly: System.Reflection.AssemblyProductAttribute("KwFeeds")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KwFeeds")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
