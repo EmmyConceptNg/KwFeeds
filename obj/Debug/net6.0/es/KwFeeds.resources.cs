9791d0135dd68f97276f1ee2efafa2c5bf222bf7
Emmy <emmyconceptng@gmail.com>
2024-08-07 01:25:32 +0100

    before applying singleproduct to homepage

--- obj/Debug/net6.0/es/KwFeeds.resources.cs
+++ obj/Debug/net6.0/es/KwFeeds.resources.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KwFeeds")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute(" ")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1209131ae81fee386e8c186f00a2fda554c736dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8806f77449dde5870a09de082dad71cea29392ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("KwFeeds")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KwFeeds")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
