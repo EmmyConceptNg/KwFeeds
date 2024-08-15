9dcce3972b73d3a83d318543e7ae20922d5175da
Emmy <emmyconceptng@gmail.com>
2024-08-15 13:34:32 +0100

    product page

--- obj/Debug/net6.0/KwFeeds.AssemblyInfo.cs
+++ obj/Debug/net6.0/KwFeeds.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KwFeeds")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2bc6db353b5d3f02f888c61365aa960354b5dbe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7076f991528167d1ac97f77fa912ba9dd1caea2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("KwFeeds")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KwFeeds")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
